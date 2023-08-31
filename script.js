2ac4de875f5a8ca2e32ba7cdccbc123e9c8734b7
MiranaMiriyeva <arzumiri20@gmail.com>
2023-08-31 13:22:00 +0400

    todolist

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-document.querySelector('#add').onclick = function (){
+document.querySelector('#add').onclick = function () {
     let input = document.querySelector('#inputId');
-if (input.value.length > 0) 
-{ document.querySelector('#container').innerHTML += `
-<div >
-  <div class="main" >
-  <span class="text">${document.querySelector('#inputId').value}</span>
-  <div class="buttons">
-  <button class="deleteBtn">Delete</button>
-  <button class="editBtn">Edit</button></div></div>
-  <hr class="hr">
-</div>
-`;
-document.querySelector('.deleteBtn').onclick = function (){
-let a=document.querySelector('.deleteBtn').innerHTML
-    for(let i=0;i<a.length;i++){
-        a[i]==0;
-        this.parentNode.removeChild();
+    if (input.value.length > 0) {
+        let container = document.querySelector('#container');
+        let newItem = document.createElement('div');
+        newItem.innerHTML += `
+            <div class="main">
+                <span class="text">${input.value}</span>
+                <div class="buttons">
+                    <button class="deleteBtn">Delete</button>
+                    <button class="editBtn">Edit</button>
+                </div>
+            </div>
+            <hr class="hr">
+        `;
+
+        
+        let deleteBtn = newItem.querySelector('.deleteBtn');
+        deleteBtn.onclick = function () {
+            container.removeChild(newItem);
+        };
+
+        
+        container.appendChild(newItem);
+
+        
+        input.value = '';
+    } else {
+        alert("Please enter text");
     }
-}
-}  else { alert("Please enter text")}}
+};
